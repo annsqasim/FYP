3c69c3ea15fdc3a29de386f59a5ebec64759c6e3
Nabiha-Arif <nabihapolani@gmail.com>
2014-04-09 13:22:05 +0500

    hellooooohellooooooooooo

---
+++ WFTest1/WFTest1/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WFTest1.Models;
+namespace WFTest1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private WFTest1Entities db = new WFTest1Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult InitiateProcess()
+        {
+            return View();
+        }
+        public ActionResult InitiatedWorkflows()
+        {
+            return View();
+        }
+
+        public ActionResult InvolvedWorkflow()
+        {
+            return View();
+        }
+
+        public ActionResult WorkflowRecords()
+        {
+            return View();
+        }
+        public ActionResult DeleteDept()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult AddDesig()
+        {
+            return View();
+        }
+
+        public ActionResult Department()
+        {
+            return View();
+        }
+        
+        //[HttpPost]
+
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Department(Department department)
+        //{
+        //    //if (ModelState.IsValid)
+        //    //{
+        //        //db.Departments.Add(department);
+        //        //db.SaveChanges();
+        //        return View("abc");
+        //    //}
+
+        //    //return View(department);
+        //}
+
+
+
+
+    }
+}
---
+++ WFTest1/WFTest1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WFTest1.MvcApplication" Language="C#" %>

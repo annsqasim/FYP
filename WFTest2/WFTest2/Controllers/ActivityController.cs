3aeac4df6db3673168994f5b32f1814eec6c32fc
Nabiha-Arif <nabihapolani@gmail.com>
2014-04-10 11:04:23 +0500

    Testing project

---
+++ WFTest2/WFTest2/Controllers/ActivityController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WFTest2.Models;
+
+namespace WFTest2.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private WFTestingEntities db = new WFTestingEntities();
+
+        //
+        // GET: /Activity/
+
+        public ActionResult Index()
+        {
+            var activities = db.Activities.Include(a => a.Process);
+            return View(activities.ToList());
+        }
+
+        //
+        // GET: /Activity/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        //
+        // GET: /Activity/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Process_Id = new SelectList(db.Processes, "Process_Id", "Proces_Name");
+            return View();
+        }
+
+        //
+        // POST: /Activity/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Activities.Add(activity);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Process_Id = new SelectList(db.Processes, "Process_Id", "Proces_Name", activity.Process_Id);
+            return View(activity);
+        }
+
+        //
+        // GET: /Activity/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Process_Id = new SelectList(db.Processes, "Process_Id", "Proces_Name", activity.Process_Id);
+            return View(activity);
+        }
+
+        //
+        // POST: /Activity/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Process_Id = new SelectList(db.Processes, "Process_Id", "Proces_Name", activity.Process_Id);
+            return View(activity);
+        }
+
+        //
+        // GET: /Activity/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        //
+        // POST: /Activity/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Activity activity = db.Activities.Find(id);
+            db.Activities.Remove(activity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WFTest2/WFTest2/Controllers/FormController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WFTest2.Models;
+
+namespace WFTest2.Controllers
+{
+    public class FormController : Controller
+    {
+        private WFTestingEntities db = new WFTestingEntities();
+
+        //
+        // GET: /Form/
+
+        public ActionResult Index()
+        {
+            return View(db.Forms.ToList());
+        }
+
+        //
+        // GET: /Form/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        //
+        // GET: /Form/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Form/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Forms.Add(form);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(form);
+        }
+
+        //
+        // GET: /Form/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        //
+        // POST: /Form/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(form).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(form);
+        }
+
+        //
+        // GET: /Form/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        //
+        // POST: /Form/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Form form = db.Forms.Find(id);
+            db.Forms.Remove(form);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
